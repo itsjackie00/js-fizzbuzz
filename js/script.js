fb289cf0f4b84866830b61a4b7f0770a0ab58da0
itsjackie00 <jackiedanso33@gmail.com>
2024-03-08 16:10:30 +0100

    fizzbuzz solution in log

--- js/script.js
+++ js/script.js
@@ -24,9 +24,18 @@ console.log('Buzz');
 CODICE DA ESEGUIRE PRIMA
 */
 
-for(i = 1; i <= 100; i++){
-    // console.log(i);
+// FizzBuzz Solution in log
 
+for (i = 1; i <= 100; i++) {
+    if (i % 15 === 0) {
+        console.log('Fizzbuzz');
+    } else if (i % 3 === 0) {
+        console.log('Fizz');
+    } else if (i % 5 === 0) {
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
 }
 
 
